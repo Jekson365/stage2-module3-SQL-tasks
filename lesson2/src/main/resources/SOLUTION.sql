9717074f80f20967e655fad2f67cdae0c2b9b9bc
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-08-12 11:56:18 +0400

    qdas

--- lesson2/src/main/resources/SOLUTION.sql
+++ lesson2/src/main/resources/SOLUTION.sql
@@ -1,52 +1,34 @@
 INSERT INTO Student (id, name, group_id) VALUES
     (1, 'John', 1),
     (2, 'Chris', 1),
-    (3, 'Carl', 1);
-
-INSERT INTO Student (id, name, group_id) VALUES
+    (3, 'Carl', 1),
     (4, 'Oliver', 2),
     (5, 'James', 2),
     (6, 'Lucas', 2),
-    (7, 'Henry', 2);
-
-INSERT INTO Student (id, name, group_id) VALUES
+    (7, 'Henry', 2),
     (8, 'Jacob', 3),
     (9, 'Logan', 3);
 
-
-INSERT INTO Subject (id, name, grade) VALUES
-    (1, 'Art and music', 1);
-
-INSERT INTO Subject (id, name, grade) VALUES
-    (2, 'Geography', 2),
-    (3, 'History', 2);
-
-INSERT INTO Subject (id, name, grade) VALUES
-    (4, 'PE', 3),
-    (5, 'Math', 3);
-
-INSERT INTO Subject (id, name, grade) VALUES
-    (6, 'Science', 4),
-    (7, 'IT', 4);
-
-INSERT INTO Subject (id, name, grade) VALUES
-    (8, 'Subject 1', 5),
-    (9, 'Subject 2', 5);
-
+INSERT INTO Subject (id, name, description, grade) VALUES
+    (1, 'Art and music', 'Art and music subject', 1),
+    (2, 'Geography', 'Geography subject', 2),
+    (3, 'History', 'History subject', 2),
+    (4, 'PE', 'Physical Education subject', 3),
+    (5, 'Math', 'Math subject', 3),
+    (6, 'Science', 'Science subject', 4),
+    (7, 'IT', 'Information Technology subject', 4);
 INSERT INTO PaymentType (id, name) VALUES
     (1, 'DAILY'),
     (2, 'WEEKLY'),
     (3, 'MONTHLY');
 
 INSERT INTO Payment (id, type_id, amount, student_id, payment_date) VALUES
-    (1, 2, 50.00, 1, NOW()),  -- John - WEEKLY
-    (2, 3, 120.00, 4, NOW()), -- Oliver - MONTHLY
-    (3, 2, 55.00, 7, NOW()),  -- Henry - WEEKLY
-    (4, 1, 10.00, 5, NOW());  -- James - DAILY
-
+    (1, 2, 100.00, 1, NOW()),
+    (2, 3, 500.00, 4, NOW()),
+    (3, 2, 150.00, 7, NOW()),
+    (4, 1, 10.00, 5, NOW());
 INSERT INTO Mark (id, student_id, subject_id, mark) VALUES
-    (1, 2, 1, 8),  -- Chris - Art
-    (2, 4, 3, 5),  -- Oliver - History
-    (3, 5, 2, 9),  -- James - Geography
-    (4, 8, 5, 4),  -- Jacob - Math
-    (5, 9, 4, 9);  -- Logan - PE
+    (1, 2, 1, 8),
+    (2, 4, 3, 5),
+    (3, 5, 2, 9),
+    (4, 8,
--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -1,18 +1,19 @@
 ALTER TABLE Student
-MODIFY birthday DATE NOT NULL;
+ALTER COLUMN birthday DATE NOT NULL;
 
 ALTER TABLE Mark
-MODIFY mark INT CHECK (mark >= 1 AND mark <= 10) NOT NULL,
-MODIFY student_id BIGINT NOT NULL,
-MODIFY subject_id BIGINT NOT NULL;
+ADD CONSTRAINT chk_mark_range CHECK (mark >= 1 AND mark <= 10);
+ALTER TABLE Mark
+ALTER COLUMN student_id BIGINT NOT NULL,
+ALTER COLUMN subject_id BIGINT NOT NULL;
 
 ALTER TABLE Subject
-MODIFY grade INT CHECK (grade >= 1 AND grade <= 5) NOT NULL;
+ADD CONSTRAINT chk_grade_range CHECK (grade >= 1 AND grade <= 5);
 
 ALTER TABLE PaymentType
-ADD CONSTRAINT unique_payment_type_name UNIQUE (name);
+ADD CONSTRAINT unique_name UNIQUE (name);
 
 ALTER TABLE Payment
-MODIFY type_id BIGINT NOT NULL,
-MODIFY amount DECIMAL NOT NULL,
-MODIFY payment_date DATETIME NOT NULL;
+ALTER COLUMN type_id BIGINT NOT NULL,
+ALTER COLUMN amount DECIMAL NOT NULL,
+ALTER COLUMN payment_date DATETIME NOT NULL;
