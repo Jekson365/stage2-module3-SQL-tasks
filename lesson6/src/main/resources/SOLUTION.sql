5d35dc7ccee451d068cf4489e8035c3d08589827
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-08-15 10:29:42 +0400

    qdas

--- lesson6/src/main/resources/SOLUTION.sql
+++ lesson6/src/main/resources/SOLUTION.sql
@@ -1,12 +1,4 @@
 SELECT * FROM Payment WHERE type_id = (SELECT id FROM PaymentType WHERE name = 'MONTHLY');
 SELECT * FROM Mark WHERE subject_id = (SELECT id FROM Subject WHERE name = 'Art and music');
-SELECT DISTINCT s.*
-FROM Student s
-JOIN Payment p ON s.id = p.student_id
-JOIN PaymentType pt ON p.type_id = pt.id
-WHERE pt.name = 'WEEKLY';
-SELECT DISTINCT s.*
-FROM Student s
-JOIN Mark m ON s.id = m.student_id
-JOIN Subject sub ON m.subject_id = sub.id
-WHERE sub.name = 'Math';
+SELECT DISTINCT s.* FROM Student s JOIN Payment p ON s.id = p.student_id JOIN PaymentType pt ON p.type_id = pt.id WHERE pt.name = 'WEEKLY';
+SELECT DISTINCT s.* FROM Student s JOIN Mark m ON s.id = m.student_id JOIN Subject sub ON m.subject_id = sub.id WHERE sub.name = 'Math';
